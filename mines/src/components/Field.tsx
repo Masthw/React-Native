7e788581edd66a896a66832a7dac359e31c0728c
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-01-28 15:21:24 -0300

    :sparkles: feat: update field component

--- mines/src/components/Field.tsx
+++ mines/src/components/Field.tsx
@@ -1,29 +1,77 @@
 import React from 'react';
-import { View, StyleSheet, StyleProp, ViewStyle } from 'react-native';
+import {View, StyleSheet, StyleProp, ViewStyle, Text} from 'react-native';
 import params from '../params';
 
-const Field: React.FC = () => {
-    const styleField: StyleProp<ViewStyle>[] = [styles.field];
-    if (styleField.length === 1) {styleField.push(styles.regular);}
+interface FieldProps {
+  mined?: boolean;
+  opened?: boolean;
+  nearMines?: number;
+}
 
-    return (
-        <View style={styleField} />
-    );
+const Field: React.FC<FieldProps> = ({
+  mined = false,
+  opened = false,
+  nearMines = 0,
+}) => {
+  const styleField: StyleProp<ViewStyle>[] = [styles.field];
+
+  if (opened) {
+    styleField.push(styles.opened);
+  }
+  if (styleField.length === 1) {
+    styleField.push(styles.regular);
+  }
+
+  let color;
+  if (nearMines > 0) {
+    if (nearMines === 1) {
+      color = '#2a28d7';
+    }
+    if (nearMines === 2) {
+      color = '#2b520f';
+    }
+    if (nearMines > 2 && nearMines < 6) {
+      color = '#F9060A';
+    }
+    if (nearMines >= 6) {
+      color = '#f221a9';
+    }
+  }
+
+  return (
+    <View style={styleField}>
+      {!mined && opened && nearMines > 0 ? (
+        <Text style={[styles.label, {color}]}>{nearMines}</Text>
+      ) : (
+        false
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    field: {
-        height: params.blockSize,
-        width: params.blockSize,
-        borderWidth: params.borderSize,
-    },
-    regular: {
-        backgroundColor: '#999',
-        borderLeftColor: '#ccc',
-        borderTopColor: '#ccc',
-        borderRightColor: '#333',
-        borderBottomColor: '#333',
-    },
+  field: {
+    height: params.blockSize,
+    width: params.blockSize,
+    borderWidth: params.borderSize,
+  },
+  regular: {
+    backgroundColor: '#999',
+    borderLeftColor: '#ccc',
+    borderTopColor: '#ccc',
+    borderRightColor: '#333',
+    borderBottomColor: '#333',
+  },
+  opened: {
+    backgroundColor: '#999',
+    borderColor: '#777',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: params.fontSize,
+  },
 });
 
 export default Field;
