0a88b337f03e13b5b67d3135a275ee0d9942db7c
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-01-29 14:32:48 -0300

    :sparkles: feat: create mine field component

---
+++ mines/src/components/MineField.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Field from './Field';
+
+
+type Cell = {
+  row: number;
+  column: number;
+  opened: boolean;
+  flagged: boolean;
+  mined: boolean;
+  exploded: boolean;
+  nearMines: number;
+};
+
+type Board = Cell[][];
+
+interface MinefieldProps {
+  board: Board;
+}
+
+const Minefield: React.FC<MinefieldProps> = ({ board }) => {
+  return (
+    <View style={styles.container}>
+      {board.map((row, r) => (
+        <View key={r} style={styles.row}>
+          {row.map((field, c) => (
+            <Field {...field} key={`${r}-${c}`} />
+          ))}
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#eee',
+    flexDirection: 'column', // Empilhar as linhas corretamente
+  },
+  row: {
+    flexDirection: 'row', // As células dentro da linha ficam lado a lado
+  },
+});
+
+export default Minefield;
