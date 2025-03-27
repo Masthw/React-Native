62a5adbde7b4c24a0bbc91447e49992b7bb036fc
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-03-27 14:43:09 -0300

    :sparkles: feat: create new app and basic views

---
+++ rnCrud/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import UserList from './views/UserList';
+import UserForm from './views/UserForm';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="UserList">
+        <Stack.Screen name="UserList" component={UserList} />
+        <Stack.Screen name="UserForm" component={UserForm} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ rnCrud/src/views/UserList.tsx
@@ -0,0 +1,13 @@
+// UserList.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const UserList = () => {
+  return (
+    <View>
+      <Text>Tela de Lista de Usuários</Text>
+    </View>
+  );
+};
+
+export default UserList;
