b319866eda4a8ab4ef3d84efdb37e7126791a661
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-03-28 14:29:22 -0300

    :sparkles: feat: create userList view

---
+++ rnCrud/src/data/users.ts
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: 'João Silva',
+    email: 'josil@email.com',
+    avatar:
+      'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+  },
+  {
+    id: 2,
+    name: 'Silva',
+    email: 'mago@email.com',
+    avatar:
+      'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+  },
+  {
+    id: 3,
+    name: 'João',
+    email: 'tanso@email.com',
+    avatar:
+      'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+  },
+  {
+    id: 4,
+    name: 'Mini Messi',
+    email: 'gol@email.com',
+    avatar:
+      'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+  },
+];
--- rnCrud/src/views/UserList.tsx
+++ rnCrud/src/views/UserList.tsx
@@ -1,11 +1,66 @@
-// UserList.js
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, FlatList, Alert} from 'react-native';
+import users from '../data/users';
+import {ListItem, Avatar} from 'react-native-elements';
+import {Button} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const UserList = () => {
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  avatar: string;
+}
+
+const UserList = ({navigation}: {navigation: any}) => {
+  const getActions = (user: User) => {
+    return (
+      <>
+        <Button
+          icon={<Icon name="edit" size={25} color="orange" />}
+          type="clear"
+          onPress={() => navigation.navigate('UserForm', {user})}
+        />
+        <Button
+          icon={<Icon name="delete" size={25} color="red" />}
+          type="clear"
+          onPress={() => confirmDelete(user)}
+        />
+      </>
+    );
+  };
+
+  const confirmDelete = (user: User) => {
+    Alert.alert('Excluir Usuário', `Deseja excluir ${user.name}?`, [
+      {text: 'Cancelar', style: 'cancel'},
+      {
+        text: 'Excluir',
+        onPress: () => console.log('Usuário excluído:', user.id),
+        style: 'destructive',
+      },
+    ]);
+  };
+
+  function getUserItem({item}: {item: User}) {
+    return (
+      <ListItem bottomDivider>
+        <Avatar source={{uri: item.avatar}} />
+        <ListItem.Content>
+          <ListItem.Title>{item.name}</ListItem.Title>
+          <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
+        </ListItem.Content>
+        {getActions(item)}
+      </ListItem>
+    );
+  }
+
   return (
     <View>
-      <Text>Tela de Lista de Usuários</Text>
+      <FlatList
+        data={users}
+        renderItem={getUserItem}
+        keyExtractor={item => item.id.toString()}
+      />
     </View>
   );
 };
