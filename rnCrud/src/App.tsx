1743959355b6140c92a1dd94f33deb6df4a1fd9d
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-03-28 11:01:27 -0300

    :sparkles: feat: add stack navigation to app

--- rnCrud/src/App.tsx
+++ rnCrud/src/App.tsx
@@ -1,20 +1,56 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator, StackNavigationOptions } from '@react-navigation/stack';
 import UserList from './views/UserList';
 import UserForm from './views/UserForm';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="UserList">
-        <Stack.Screen name="UserList" component={UserList} />
-        <Stack.Screen name="UserForm" component={UserForm} />
+      <Stack.Navigator initialRouteName="UserList" screenOptions={screenOptions}>
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={({ navigation }) => ({
+            title: 'Lista de Usuários',
+            headerRight: () => <AddUserButton navigation={navigation} />,
+          })}
+        />
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{ title: 'Formulário de Usuários' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
+};
+
+interface AddUserButtonProps {
+  navigation: any;
+}
+
+const AddUserButton: React.FC<AddUserButtonProps> = ({ navigation }) => (
+  <Button
+    type="clear"
+    icon={<Icon name="add" size={25} color="white" />}
+    onPress={() => navigation.navigate('UserForm')}
+  />
+);
+
+const screenOptions: StackNavigationOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold' as 'bold',
+  },
 };
 
 export default App;
