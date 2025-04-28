0a3444b9f64da2a24d47c9883c3df6225d0360b2
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-04-28 10:45:56 -0300

    :sparkles: feat: add user to header

--- lambe/src/components/Header.tsx
+++ lambe/src/components/Header.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import {StyleSheet, Text, View, Platform, Image} from 'react-native';
 import icon from '../../assets/imgs/icon.png';
+import {connect} from 'react-redux';
+import {Gravatar} from 'react-native-gravatar';
+import {User} from '../types/User';
 
-const Header = () => {
+interface Props {
+  user: User;
+}
+
+const Header: React.FC<Props> = ({user}) => {
+  const gravatarOptions = {email: user?.email || '', secure: true};
   return (
     <View style={styles.container}>
       <View style={styles.rowContainer}>
-        <Image source={icon} style={styles.image} />
-        <Text style={styles.title}>Lambe lambe</Text>
+        <View style={styles.leftContainer}>
+          <Image source={icon} style={styles.image} />
+          <Text style={styles.title}>Lambe lambe</Text>
+        </View>
+        <View style={styles.rightContainer}>
+          <Text style={styles.username}>{user?.name}</Text>
+          <Gravatar options={gravatarOptions} style={styles.avatar} />
+        </View>
       </View>
     </View>
   );
@@ -23,6 +37,15 @@ const styles = StyleSheet.create({
   rowContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  leftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  rightContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   image: {
     height: 30,
@@ -35,6 +58,21 @@ const styles = StyleSheet.create({
     height: 30,
     fontSize: 28,
   },
+  avatar: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    marginLeft: 10,
+  },
+  username: {
+    color: '#888',
+    fontSize: 12,
+    marginLeft: 5,
+  },
 });
 
-export default Header;
+const mapStateToProps = (state: any) => ({
+  user: state.user,
+});
+
+export default connect(mapStateToProps)(Header);
